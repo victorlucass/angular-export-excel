c49c5734a672e0799f2f443bd36c8f0f4c355cff
victor lucas <victorlucas.ao@gmail.com>
2021-06-28 22:13:52 -0400

    feat: complet export xlsx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
+import { ExporterExcelService } from './service/exporter-excel.service';
+import { MaterialModule } from './shared/material/material.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DataTableComponent } from './components/data-table/data-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
-  providers: [],
+  providers: [ExporterExcelService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
